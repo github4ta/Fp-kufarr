f343780b06466f0a1b09caafe47d4927dac02696
Sergei Tsarik <serge.tsarik@gmail.com>
2025-07-22 23:47:58 +0300

    add comments

--- src/test/java/by/kufar/ui/LoginUITest.java
+++ src/test/java/by/kufar/ui/LoginUITest.java
@@ -9,7 +9,7 @@ public class LoginUITest extends BaseUITest {
     @Test
     public void testSuccessfulLogin() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("valid@email.com", "validPassword");
+        loginPage.login("valid@email.com", "validPassword"); // "valid@email.com" и "validPassword" скорее всего нужно заменить на зарегистрированного пользователя и тогда тест будет работать. но мы такую проверку не делаем.
         Assert.assertTrue(loginPage.isLoggedIn(), "Login failed");
     }
 
@@ -28,4 +28,6 @@ public class LoginUITest extends BaseUITest {
         Assert.assertEquals("", "Заполните обязательное поле");
         Assert.assertEquals("", "Введите пароль");
     }
+
+    // и .т.д. тесты которые проверяют форму логина для незарегистрированного пользователя, или с некорректными данными, или с пустыми данными. и смотрим (проверяем) что соответствующие тексты сообщений появляются.
 }
